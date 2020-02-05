819321d606fb2d1da301944adc0b7e39d41f451b
I.Y <iyushi1010@gmail.com>
2020-02-05 02:50:20 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -22,3 +26,4 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
