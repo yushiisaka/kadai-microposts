113361d3e094bca8b2afe514b53df08b730f629c
yushiisaka <iyushi1010@gmail.com>
2020-02-16 10:44:59 +0000

    addfav2

--- database/migrations/2020_02_12_091441_create_favorites_table.php
+++ database/migrations/2020_02_12_091441_create_favorites_table.php
@@ -23,7 +23,7 @@ class CreateFavoritesTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('micropost_id')->references('id')->on('microposts')->onDelete('cascade');
             
-            $table->unique(['user_id', 'microposts_id']);
+            $table->unique(['user_id', 'micropost_id']);
         });
     }
 
